a115e477fec1609da83aaa1b9d51bedfaf20ee95
Kirti Sharma <kirti2001sharma@gmail.com>
2022-10-09 22:57:57 +0530

    feat: added header and banner component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
 
-import React from 'react'
+// materialUI hook for custom CSS ⬇
+const useStyles = makeStyles({
+  app: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh"
+  },
+})
 
 const App = () => {
+
+  const classes = useStyles(); //object of class `useStyles`
+
   return (
-    <div>App</div>
-  )
+      <div className={classes.app}>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+      <Route  path='/' element={<Homepage/>} />
+      <Route path= '/coins/:id' element={<CoinPage/>} />
+      </Routes>
+      </BrowserRouter>
+      </div>
+    
+  );
 }
 
 export default App
-
 
